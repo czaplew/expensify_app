223994d4cf5a6150894c5d5ee8be2047dafa597d
czaplew <kczaplew0@gmail.com>
2018-02-24 14:18:10 +0100

    Heroku config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,43 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = merge(common, {
-  plugins: [new UglifyJSPlugin()]
+
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: [/\.s?css$/],
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  },
+
+  plugins: [new UglifyJSPlugin(), new ExtractTextPlugin("style.css")]
 });
